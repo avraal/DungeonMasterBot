98ea9464825cbe10aeb364f84dd04fe2e04efb90
avolsky <avolsky@superstream.com>
2018-08-03 12:05:11 +0300

    add link to git

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version=src.__version__,
     packages=find_packages(),
     author='Andrew Volski',
+    url='https://github.com/avraal/DungeonMasterBot',
     author_email='andrew.volski@gmail.com',
     install_requires=['discord'],
     description='Simple Discord bot for help players'
