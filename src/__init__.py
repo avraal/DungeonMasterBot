4da705c88b786cbaefe960cc728d4bbd323d3024
avraal <gammaclone@gmail.com>
2018-11-28 18:55:04 +0200

    show private information to public

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '2.1'
+__TOKEN_BAKA_ = 'MzQ3MDIwNDkwMjQzODMzODU2.DkWVFQ.1htEWV0wXMm8YZ2603pmy52QVIQ'
+__TOKEN_MASTER_ = 'NDc0NDg0OTkxNDg1NjczNDcy.DkWVFQ.uqwFXRPnHbuzvjS7XtVlYXWhzrc'
+
+__exec_command_url__ = 'http://taskbox.zzz.com.ua/execCommand.php'
+__create_dnd_module__url__ = 'http://taskbox.zzz.com.ua/create_dnd_module.php'
+__create_dnd_user_url__ = 'http://taskbox.zzz.com.ua/create_dnd_user.php'
+__get_dnd_modules_url__ = 'http://taskbox.zzz.com.ua/get_dnd_modules_by_user.php'
+__get_dnd_users_url__ = 'http://taskbox.zzz.com.ua/get_users_by_module.php'
